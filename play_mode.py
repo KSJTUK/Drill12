963b4e2e99f3d6e4a6ea2be36cf9aaeb1b41b48b
KimsungjoonMyLaptop <lastbabo2480@tukorea.ac.kr>
2023-11-06 11:57:52 +0900

    게임 월드에 좀비 추가

--- play_mode.py
+++ play_mode.py
@@ -46,6 +46,9 @@ def init():
     for ball in balls:
         game_world.add_collision_pair('boy:ball', None, ball)
 
+    zombies = [Zombie() for _ in range(5)]
+    game_world.add_objects(zombies, 1)
+
 
 def finish():
     game_world.clear()
